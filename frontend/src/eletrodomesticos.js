8b4e9f1d45b3e1bc2c4fa82e1a9aff1a8474703e
Tanjil <tanjilkh@gmail.com>
2024-09-18 11:49:15 +0100

    Update eletrodomesticos.js
    acrescentar imagem

--- frontend/src/eletrodomesticos.js
+++ frontend/src/eletrodomesticos.js
@@ -2,6 +2,7 @@ const eletrodomesticos = [
     {
         _id: 1,
         nome: 'Máquina De Lavar Roupa Qilive Q.6631',
+        imagem: '/images/quilive-q6631.jpg',
         descricao: 'Máquina de Lavar Roupa QILIVE com capacidade para 6KG. Velocidade máxima de centrifugação 1200rpm e com 23 programas ',
         preco: 239.99,
         emStock: 10,
@@ -11,6 +12,7 @@ const eletrodomesticos = [
     {
         _id: 2,
         nome: 'Frigorífico BECKEN BDD5394 WH',
+        imagem: '/images/becken-bdd5394-wh.jpg',
         descricao: 'Frigorífico BECKEN BDD5394 WH (No Frost - 167.6 cm - 246 L - Branco)',
         preco: 319.99,
         emStock: 15,

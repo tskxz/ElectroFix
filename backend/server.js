8c363e226bcea7a3ecd77e350cd1045bb1b70674
Tanjil <tanjilkh@gmail.com>
2024-09-22 10:31:16 +0100

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+const port = 3000;
+import eletrodomesticos from './data/eletrodomesticos.js';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Api está a rodar...');
+})
+
+app.get('/api/eletrodomesticos', (req, res) => {
+    res.json(eletrodomesticos);
+});
+
+app.get('/api/eletrodomesticos/:id', (req, res) => {
+    const eletrodomestico = eletrodomesticos.find(e => e._id === req.params.id);
+    res.json(eletrodomestico);
+})
+
+app.listen(port, () => {
+    console.log(`Servidor a rodar na porta ${port}`);
+});

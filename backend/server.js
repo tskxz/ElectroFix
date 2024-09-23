50ce769fb8ad1b61815549a7c1827a07ae3699fa
Tanjil <tanjilkh@gmail.com>
2024-09-23 11:04:18 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import eletrodomesticos from './data/eletrodomesticos.js';
 import connectDB from './config/db.js';
+import eletrodomesticoRoutes from './routes/eletrodomesticoRoutes.js';
 
 dotenv.config();
 
 const port = process.env.PORT || 5000;
 const app = express();
+
 connectDB();
+
+app.use('/api/eletrodomesticos', eletrodomesticoRoutes);
 
 app.get('/', (req, res) => {
     res.send('Api está a rodar...');
 })
 
-app.get('/api/eletrodomesticos', (req, res) => {
-    res.json(eletrodomesticos);
-});
-
-app.get('/api/eletrodomesticos/:id', (req, res) => {
-    const eletrodomestico = eletrodomesticos.find(e => e._id === req.params.id);
-    res.json(eletrodomestico);
-})
 
 app.listen(port, () => {
     console.log(`Servidor a rodar na porta ${port}`);

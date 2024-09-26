ce72f9f26610d998fcdb5d12513790349622096b
Tanjil <tanjilkh@gmail.com>
2024-09-26 20:41:37 +0100

    Update EletrodomesticoScreen.jsx

--- frontend/src/Screens/EletrodomesticoScreen/EletrodomesticoScreen.jsx
+++ frontend/src/Screens/EletrodomesticoScreen/EletrodomesticoScreen.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
-import {useParams} from 'react-router-dom'
-import {Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap';
+import {useParams, useNavigate} from 'react-router-dom'
+import {Row, Col, Image, ListGroup, Card, Button, Form} from 'react-bootstrap';
 import Rating from '../../components/Rating'
 import {useState, useEffect} from 'react'
 import axios from 'axios';
 import { useGetEletrodomesticoQuery } from '../../slices/eletrodomesticosApiSlice';
 import Loader from '../../components/Loader'
 import Message from '../../components/Message';
+import {useDispatch} from 'react-redux'
+import { adicionarToCarrinho } from '../../slices/carrinhoSlice';
 
 
 const EletrodomesticoScreen = () => {
     const {id: eletrodomesticoId} = useParams()
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const [quantidade, setQuantidade] = useState(1);
     const {data: eletrodomestico, isLoading, error} = useGetEletrodomesticoQuery(eletrodomesticoId);
+    const adicionarToCarrinhoHandler = () => {
+      dispatch(adicionarToCarrinho({...eletrodomestico, quantidade}))
+      navigate('/carrinho')
+    }
     return(
       <>
       {isLoading ? (<Loader/>) : error ? (<Message variant='danger'>{error?.data?.message || error.error}</Message>) : (<>
@@ -54,9 +63,22 @@ const EletrodomesticoScreen = () => {
                 </Col>
               </Row>
             </ListGroup.Item>
+
+            {eletrodomestico.emStock > 0 && (
+              <ListGroup.Item>
+                <Row>
+                  <Col>Quantidade</Col>
+                  <Col>
+                    <Form.Control as='select' value={quantidade} onChange={(e) => setQuantidade(Number(e.target.value))}>
+                      {[...Array(eletrodomestico.emStock).keys()].map((x) => (<option key={x+1} value={x+1}>{x+1}</option>))}
+                    </Form.Control>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            )}
             
             <ListGroup.Item>
-              <Button className='btn-block' type='button' disabled={eletrodomestico.emStock === 0}>Adicionar ao carrinho</Button>
+              <Button className='btn-block' type='button' disabled={eletrodomestico.emStock === 0} onClick={adicionarToCarrinhoHandler}>Adicionar ao carrinho</Button>
             </ListGroup.Item>
           </ListGroup>
         </Card>

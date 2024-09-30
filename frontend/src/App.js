5137f528caddc999ff61afc1d4843577beebe3e5
Tanjil <tanjilkh@gmail.com>
2024-09-30 11:11:39 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Footer from './components/Footer'
 import { Outlet } from 'react-router-dom';
 import {ToastContainer} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+
 const App = () => {
   return (
     <>

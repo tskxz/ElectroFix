256fd481236c381ab33caf404b320a16ea9fe328
Tanjil <tanjilkh@gmail.com>
2024-10-02 23:54:10 +0100

    Update eletrodomesticoController.js

--- backend/controllers/eletrodomesticoController.js
+++ backend/controllers/eletrodomesticoController.js
@@ -22,4 +22,41 @@ const getEletrodomestico = asyncHandler(async (req, res) => {
     }
 })
 
-export {getEletrodomesticos, getEletrodomestico}
+// @desc  Criar um eletrodoméstico
+// @route POST /api/eletrodomesticos
+// @access Private/Admin
+const criarEletrodomestico = asyncHandler(async(req, res) => {
+    const eletrodomestico = new Eletrodomestico({
+        nome: 'Nome',
+        preco: 0,
+        utilizador: req.utilizador._id,
+        imagem: '/images/sample.jpg',
+        emStock: 0,
+        numReviews: 0,
+        descricao: 'Descricao'
+    })
+    const eletrodomesticoCriado = await eletrodomestico.save()
+    res.json(eletrodomesticoCriado)
+})
+
+// @desc  Atualizar um eletrodoméstico
+// @route PUT /api/eletrodomesticos/:id
+// @access Private/Admin
+const atualizarEletrodomestico = asyncHandler(async(req, res) => {
+    const {nome, preco, emStock, numReviews, descricao} = req.body
+    const eletrodomestico = await Eletrodomestico.findById(req.params.id)
+    if(eletrodomestico){
+        eletrodomestico.nome = nome || eletrodomestico.nome
+        eletrodomestico.preco = preco || eletrodomestico.preco
+        eletrodomestico.emStock = emStock || eletrodomestico.emStock
+        eletrodomestico.numReviews = numReviews || eletrodomestico.numReviews
+        eletrodomestico.descricao = descricao || eletrodomestico.descricao
+        const eletrodomesticoAtualizado = await eletrodomestico.save()
+        res.json(eletrodomesticoAtualizado)
+    } else {
+        res.status(404)
+        throw new Error('Eletrodomestico não encontrado')
+    }
+})
+
+export {getEletrodomesticos, getEletrodomestico, criarEletrodomestico, atualizarEletrodomestico}

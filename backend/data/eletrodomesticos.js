4c5158674546ebbad2da3b712d5515bc4535aca5
RicardoMaga <ricardontmagalhaes04@gmail.com>
2024-10-03 13:52:06 +0100

    Adicionar Máquina de Lavar Loiça SIEMENS

--- backend/data/eletrodomesticos.js
+++ backend/data/eletrodomesticos.js
@@ -2,7 +2,7 @@ const eletrodomesticos = [
     {
         nome: 'Máquina De Lavar Roupa Qilive Q.6631',
         imagem: '/images/quilive-q6631.jpg',
-        descricao: 'Máquina de Lavar Roupa QILIVE com capacidade para 6KG. Velocidade máxima de centrifugação 1200rpm e com 23 programas ',
+        descricao: 'Máquina de Lavar Roupa QILIVE com capacidade para 6KG. Velocidade máxima de centrifugação 1200rpm e com 23 programas',
         preco: 239.99,
         emStock: 10,
         rating: 4.5,
@@ -21,7 +21,7 @@ const eletrodomesticos = [
     {
         nome: 'Máquina de lavar roupa Samsung WW80T4040EEEP',
         imagem: '/images/samsung-ww80t40.jpg',
-        descricao: 'Capacidade de Lavagem: 8 Kgs Velocidade de Centrifugação: 1400 RPM Motor Digital Inverter Número de Programas: 12 Lavagem Rápida 15? Higienização a Vapor Limpeza do Tambor Opção de Demolhar Opção Enxaguamento+ Opção de Final Diferido Classe de Eficiência Energética: D Dimensões (AxLxP): 85 x 60 x 55 cm ',
+        descricao: 'Capacidade de Lavagem: 8 Kgs Velocidade de Centrifugação: 1400 RPM Motor Digital Inverter Número de Programas: 12 Lavagem Rápida 15? Higienização a Vapor Limpeza do Tambor Opção de Demolhar Opção Enxaguamento+ Opção de Final Diferido Classe de Eficiência Energética: D Dimensões (AxLxP): 85 x 60 x 55 cm',
         preco: 199.99,
         emStock: 5,
         rating: 4.0,
@@ -30,7 +30,7 @@ const eletrodomesticos = [
     {
         nome: 'Frigorífico Combinado Bosch Inox KGN39HIEP Kontrolsat',
         imagem: '/images/combinado-bosch-inox.jpg',
-        descricao: 'O Frigorífico Combinado Bosch KGN39HIEP (Série 6) é uma solução moderna e eficiente para a conservação de alimentos, com um design elegante em inox e uma capacidade total de 366 litros.',
+        descricao: 'O Frigorífico Combinado Bosch KGN39HIEP (Série 6) é uma solução moderna e eficiente para a conservação de alimentos, com um design elegante em inox e uma capacidade total de 366 litros',
         preco: 349.99,
         emStock: 2,
         rating: 4.5,
@@ -39,13 +39,21 @@ const eletrodomesticos = [
     {
         nome: 'Máquina de lavar roupa Whirlpool 8,0kg FFB 8458',
         imagem: '/images/Whirlpool-8kg.jpg',
-        descricao: 'A máquina de lavar roupa Whirlpool FFB 8458 é uma opção moderna e eficiente, projetada para oferecer ótimo desempenho e cuidar das suas roupas com tecnologia de ponta. ',
+        descricao: 'A máquina de lavar roupa Whirlpool FFB 8458 é uma opção moderna e eficiente, projetada para oferecer ótimo desempenho e cuidar das suas roupas com tecnologia de ponta',
         preco: 269.99,
         emStock: 5,
         rating: 4.0,
         numReviews: 13,
     },
-
+    {
+        nome: 'Máquina Lavar Loiça Siemens SN27YI03CE',
+        imagem: '/images/Máquina de Lavar Loiça SIEMENS.jpg',
+        descricao: 'A Máquina de Lavar Loiça Siemens SN27YI03CE é um modelo de alta gama, projetado para oferecer eficiência, tecnologia avançada e grande capacidade',
+        preco: 349.99,
+        emStock: 1,
+        rating: 4.5,
+        numReviews: 11,
+    },
 ]
 
 export default eletrodomesticos

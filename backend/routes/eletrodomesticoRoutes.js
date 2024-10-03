c5730c4d5f61a8c9635142703b329eceb8299f1c
Tanjil <tanjilkh@gmail.com>
2024-10-03 12:08:55 +0100

    Update eletrodomesticoRoutes.js

--- backend/routes/eletrodomesticoRoutes.js
+++ backend/routes/eletrodomesticoRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 const router  = express.Router();
-import { getEletrodomesticos, getEletrodomestico, criarEletrodomestico, atualizarEletrodomestico, deleteEletrodomestico} from '../controllers/eletrodomesticoController.js';
+import { getEletrodomesticos, getEletrodomestico, criarEletrodomestico, atualizarEletrodomestico, deleteEletrodomestico, criarEletrodomesticoReview} from '../controllers/eletrodomesticoController.js';
 import {protect, admin} from '../middleware/authMiddleware.js'
 router.get('/', getEletrodomesticos);
 router.get('/:id', getEletrodomestico);
 router.post('/', protect, admin, criarEletrodomestico);
 router.put('/:id', protect, admin, atualizarEletrodomestico);
 router.delete('/:id', protect, admin, deleteEletrodomestico);
+router.post('/:id/reviews', protect, criarEletrodomesticoReview)
 export default router

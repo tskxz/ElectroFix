320e797c0616e29cf32533cc950ab1224825e894
RicardoMaga <ricardontmagalhaes04@gmail.com>
2024-10-03 14:08:45 +0100

    Adicionar Maquina LG e dimensoes aos produtos

--- backend/data/eletrodomesticos.js
+++ backend/data/eletrodomesticos.js
@@ -30,7 +30,7 @@ const eletrodomesticos = [
     {
         nome: 'Frigorífico Combinado Bosch Inox KGN39HIEP Kontrolsat',
         imagem: '/images/combinado-bosch-inox.jpg',
-        descricao: 'O Frigorífico Combinado Bosch KGN39HIEP (Série 6) é uma solução moderna e eficiente para a conservação de alimentos, com um design elegante em inox e uma capacidade total de 366 litros',
+        descricao: 'O Frigorífico Combinado Bosch KGN39HIEP (Série 6) é uma solução moderna e eficiente para a conservação de alimentos, com um design elegante em inox e uma capacidade total de 366 litros, Dimensões do produto: 2040 x 600 x 660 mm',
         preco: 349.99,
         emStock: 2,
         rating: 4.5,
@@ -39,7 +39,7 @@ const eletrodomesticos = [
     {
         nome: 'Máquina de lavar roupa Whirlpool 8,0kg FFB 8458',
         imagem: '/images/Whirlpool-8kg.jpg',
-        descricao: 'A máquina de lavar roupa Whirlpool FFB 8458 é uma opção moderna e eficiente, projetada para oferecer ótimo desempenho e cuidar das suas roupas com tecnologia de ponta',
+        descricao: 'A máquina de lavar roupa Whirlpool FFB 8458 é uma opção moderna e eficiente, projetada para oferecer ótimo desempenho e cuidar das suas roupas com tecnologia de ponta, (AxLxP): 84.5 x 59.5 x 63.0',
         preco: 269.99,
         emStock: 5,
         rating: 4.0,
@@ -48,7 +48,7 @@ const eletrodomesticos = [
     {
         nome: 'Máquina Lavar Loiça Siemens SN27YI03CE',
         imagem: '/images/Máquina de Lavar Loiça SIEMENS.jpg',
-        descricao: 'A Máquina de Lavar Loiça Siemens SN27YI03CE é um modelo de alta gama, projetado para oferecer eficiência, tecnologia avançada e grande capacidade',
+        descricao: 'A Máquina de Lavar Loiça Siemens SN27YI03CE é um modelo de alta gama, projetado para oferecer eficiência, tecnologia avançada e grande capacidade, (AxLxP): 84.5 x 60 x 60 cm',
         preco: 349.99,
         emStock: 1,
         rating: 4.5,
@@ -57,7 +57,16 @@ const eletrodomesticos = [
     {
         nome: 'Frigorífico americano haier hsr3918enpg gentle silver 521l',
         imagem: '/images/frigorifico-americano-haier.jpg',
-        descricao: 'Com 90 cm de largura, os frigoríficos Side By Side 90 da Haier oferecem uma grande capacidade enquanto proporcionam um acesso fácil aos espaços tanto do frigorífico como do congelador.',
+        descricao: 'Com 90 cm de largura, os frigoríficos Side By Side 90 da Haier oferecem uma grande capacidade enquanto proporcionam um acesso fácil aos espaços tanto do frigorífico como do congelador, 908x1775x647 mm',
+        preco: 449.99,
+        emStock: 2,
+        rating: 4.0,
+        numReviews: 18,
+    },
+    {
+        nome: 'Máquina de lavar roupa lg f4wr5010a6w 10kg',
+        imagem: '/images/maquina-LG.jpg',
+        descricao: 'Motor Inverter Direct DriveT com Inteligência Artificial. Tratamento anti-alergias com a tecnologia SteamT. Batentes robustos e antibacterianos em aço inoxidável, 600x850x565 mm',
         preco: 449.99,
         emStock: 2,
         rating: 4.0,

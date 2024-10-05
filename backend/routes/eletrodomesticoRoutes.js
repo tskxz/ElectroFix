1f1f84bf6adb60ac308d59725d612f7aa0b1613c
TocadoPorDeus <159566566+TocadoPorDeus@users.noreply.github.com>
2024-10-05 23:22:55 +0100

    Update backend/routes/eletrodomesticoRoutes.js
    Co-authored-by: Tanjil <tanjilkh@gmail.com>

--- backend/routes/eletrodomesticoRoutes.js
+++ backend/routes/eletrodomesticoRoutes.js
@@ -3,8 +3,8 @@ const router  = express.Router();
 import { getEletrodomesticos, getEletrodomestico, criarEletrodomestico, atualizarEletrodomestico, deleteEletrodomestico, criarEletrodomesticoReview, getTopEletrodomesticos} from '../controllers/eletrodomesticoController.js';
 import {protect, admin} from '../middleware/authMiddleware.js'
 router.get('/', getEletrodomesticos);
-router.get('/:id', getEletrodomestico);
 router.get('/top', getTopEletrodomesticos)
+router.get('/:id', getEletrodomestico);
 router.post('/', protect, admin, criarEletrodomestico);
 router.put('/:id', protect, admin, atualizarEletrodomestico);
 router.delete('/:id', protect, admin, deleteEletrodomestico);

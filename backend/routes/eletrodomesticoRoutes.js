5dafb413a194d55f1534e47afbe2641ddab76e4e
TocadoPorDeus <ricardo.magalhaes.2022149@my.istec.pt>
2024-10-06 21:45:10 +0100

    Update eletrodomesticoRoutes.js

--- backend/routes/eletrodomesticoRoutes.js
+++ backend/routes/eletrodomesticoRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 const router  = express.Router();
-import { getEletrodomesticos, getEletrodomestico, criarEletrodomestico, atualizarEletrodomestico, deleteEletrodomestico, criarEletrodomesticoReview} from '../controllers/eletrodomesticoController.js';
+import { getEletrodomesticos, getEletrodomestico, criarEletrodomestico, atualizarEletrodomestico, deleteEletrodomestico, criarEletrodomesticoReview, getTopEletrodomesticos} from '../controllers/eletrodomesticoController.js';
 import {protect, admin} from '../middleware/authMiddleware.js'
 router.get('/', getEletrodomesticos);
 router.get('/:id', getEletrodomestico);
+router.get('/top', getTopEletrodomesticos)
 router.post('/', protect, admin, criarEletrodomestico);
 router.put('/:id', protect, admin, atualizarEletrodomestico);
 router.delete('/:id', protect, admin, deleteEletrodomestico);
